6ed976ddd5a87ade4eeff141e4c649ea49c26402
pranjal9424 <pranjalgupta9424@gmail.com>
2021-05-27 01:29:44 +0530

    Array 25

--- 25.cpp
+++ 25.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 int main()
 {
+    int n,k;
+    cin>>n>>k;
 
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    /// logic starting here
+    int x=n/k;
+    unordered_map<int,int>freq;
+
+    for(int i=0;i<n;i++)
+        freq[arr[i]]++;
+
+    for(auto i:freq){
+        if(i.second>x)
+            cout<<"Number-"<<i.first<<endl<<"Count-"<<i.second;
+    }
+
+    return 0;
 }
