136eee542a9b4c04303d24da9d4c98bbedd37f63
pranjal9424 <pranjalgupta9424@gmail.com>
2021-06-06 02:10:19 +0530

    string 52

---
+++ 52.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+///My approach
+bool interleaving(string str1,string str2,string res)
+{
+    int l=str1.length(),l2=str2.length(),lr=res.length();
+
+    int i=0,j=0,k=0;
+    while(k<lr)
+    {
+        if(i<l && str1[i]==res[k]) i++;
+        else    if(j<l2 && str2[j]==res[k]) j++;
+                else
+                    return false;
+        k++;
+    }
+    return true;
+}
+////GFG APPEOACH
+
+int main()
+{
+    string str1,str2,res;
+    cin>>str1>>str2;
+    cin>>res;
+
+    if(interleaving(str1,str2,res))
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
+
+
+/*
+        input:- str1 = "12"
+                str2 ="XY";
+                res = "X12Y";
+       output:- YES
+*/
+
