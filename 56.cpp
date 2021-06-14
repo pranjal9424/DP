c842643391cf35492b34554d20fad803052d5192
pranjal9424 <pranjalgupta9424@gmail.com>
2021-06-15 00:33:33 +0530

    56

---
+++ 56.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sub(string t, int i, int n,string s)
+{
+    if(i==n){
+        cout<<t<<endl;
+    }else
+    {
+        sub(t,i+1,n,s);
+        t=t+s[i];
+        sub(t,i+1,n,s);
+    }
+}
+int main()
+{
+    char seq[] = "abcd";
+    int n = strlen(seq);
+    sub("",0,n,seq);
+    return 0;
+}
