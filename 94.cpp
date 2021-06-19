e957fcee4c31479dd3e94187e05dc1d43485765b
pranjal9424 <pranjalgupta9424@gmail.com>
2021-06-19 23:55:47 +0530

    searching 94

---
+++ 94.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int sqrint(int n)
+{
+    int h=n;
+    int l=1;
+    int ans=-1;
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(mid<=n/mid)
+        {
+            ans=mid;
+            l=mid+1;
+        }
+        else {
+            h=mid-1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    cout<<sqrint(4);
+}
+
+
