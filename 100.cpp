10ca68f113e595be79d17a948d8b85f71266a965
pranjal9424 <pranjalgupta9424@gmail.com>
2021-06-23 16:11:31 +0530

    searching 100

---
+++ 100.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int i=0,j=1;
+    while(i<n && j<n){
+        if(i!=j && arr[j]-arr[i]==k)
+        {
+            cout<<arr[j]<<" "<<arr[i];
+            return 0;
+        }else if(arr[j]-arr[i]<k)
+                    j++;
+                else
+                    i++;
+    }
+    cout<<"Not found";
+    return 0;
+}
