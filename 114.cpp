b947c272d22a121265c481ee422ad12067ed5466
pranjal9424 <pranjalgupta9424@gmail.com>
2021-08-28 03:36:50 +0530

    114

---
+++ 114.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,c;
+    cin>>n>>c;
+    
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    
+    int lb=1;
+    int ub=1e9;
+    int ans=0;
+    while(lb<=ub)
+    {
+        int mid=(lb+ub)/2;
+        int cow=1;
+        int prev=a[0];
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]-prev>=mid)
+            {
+                cow++;
+                prev=a[i];
+                if(cow==c) break;
+            }
+        }
+        if(cow==c)
+        {
+            ans=mid;
+            lb=mid+1;
+        }
+        else
+        {
+            ub=mid-1;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
