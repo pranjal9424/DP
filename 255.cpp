6820c2c98b023a3b95ab89bcf70be5b23bbe3456
pranjal9424 <pranjalgupta9424@gmail.com>
2021-11-22 01:57:06 +0530

    backtrack

--- 255.cpp
+++ 255.cpp
@@ -1,16 +1,27 @@
-public:void solve(int i,int j,vector<vector<int>> &a,int n,vector<string> &ans,string moves,vector<vector<int>> &vis,int di[],int dj[]){
+void solve(int i,int j,vector<vector<int>> &a,int n,
+    vector<string> &ans,string move,vector<vector<int>> &vis,
+    int di[],int dj[]){
     if(i==n-1 && j==n-1){
         ans.push_back(move);
         return;
     }
     string dir="DLRU";
-    for(int ind=0;int<4;ind++)
-}
+    for(int ind=0;ind<4;ind++){
+        int nexti=i+di[ind];
+        int nextj=j+dj[ind];
+        if(nexti >= 0 && nextj>=0 && nexti<n && nextj<n && 
+        !vis[nexti][nextj]
+        && a[nexti][nextj]==1){
+            vis[i][j]=1;
+            solve(nexti,nextj,a,n,ans,move+dir[ind],vis,di,dj);
+            vis[i][j]=0;
+        }
+    }
+    }
     vector<string> findPath(vector<vector<int>> &m, int n) {
-        // Your code goes here
         vector<string> ans;
-        vector<vector<int>> vis(n, vector<int> (n,0));
-        int di[]={+1,0,0-1};
+        vector<vector<int>> vis(n, vector<int> (n, 0));
+        int di[]={+1,0,0,-1};
         int dj[]={0,-1,1,0};
         
         if(m[0][0]==1) solve(0,0,m,n,ans, "",vis,di,dj);
